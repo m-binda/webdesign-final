8f39c43f0b0a922a5600142e03aa9df29c6a24cd
m-binda <marcelobinda@gmail.com>
2021-08-29 12:35:26 -0300

    Implemented gallery arrows

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -2,14 +2,108 @@
 // Script for the image gallery at events.html
 // ----------------------------
 
-let photoImage = document.getElementsByClassName("gallery-image")
+// Gets an array of all images of the gallery
+let photoCollection = document.getElementsByClassName("gallery-image")
+
+// Gets large image and its enclosing div
+let largeImageBox = document.getElementById("fullImgBox");
+let largeImage = document.getElementById("fullImg");
+
+// Get the navigation elements of the gallery once the image is opened
+let leftArrow = document.getElementById("left");
+let rightArrow = document.getElementById("right");
 let closeX = document.getElementById("closeX");
 
-function openFullImg(photoSrc) {
-    document.getElementById("fullImgBox").style.display = "flex";
-    document.getElementById("fullImg").src = photoSrc;
+// Initialize empty objects for next and previous image
+let nextImage = { src: '', alt: '' };
+let prevImage = { src: '', alt: '' };
+
+// Initialize object for the last image in the gallery
+let lastImage = {
+    src: photoCollection[photoCollection.length - 1].src,
+    alt: photoCollection[photoCollection.length - 1].alt
 };
 
+// Loop to go through the photoCollection array
+for (let i = 0; i < photoCollection.length; i++) {
+
+    // Adds event listener to array
+    photoCollection[i].addEventListener("click", openLarge);
+
+}
+
+// Function to open the large image
+function openLarge() {
+    // Shows the large image
+    largeImageBox.style.display = "flex";
+
+    // Modifies the src and alt of the opened image
+    [largeImage.src, largeImage.alt] = [this.src, this.alt];
+};
+
+
+// Event listeners to move to the previous and next photos
+rightArrow.addEventListener("click", goNext);
+leftArrow.addEventListener("click", goPrev);
+
+// Function to move to the next photo
+function goNext() {
+
+    // Loops through the gallery array
+    for (let i = 0; i < photoCollection.length; i++) {
+
+        // Checks the current opened photo and if it's not the last image
+        if (largeImage.src == photoCollection[i].src &&
+            i + 1 < photoCollection.length) {
+            // Updates next image in the gallery
+            nextImage.src = photoCollection[i + 1].src;
+            nextImage.alt = photoCollection[i + 1].alt;
+        }
+
+        // Checks the current opened photo and if it's the last image
+        else if (largeImage.src == photoCollection[i].src) {
+
+            // Updates next photo to the first in the gallery
+            nextImage.src = photoCollection[0].src;
+            nextImage.alt = photoCollection[0].alt;
+        }
+    };
+
+    // Updates the opened image
+    [largeImage.src, largeImage.alt] = [nextImage.src, nextImage.alt];
+};
+
+
+// Function to move to the previous image
+function goPrev() {
+
+    // Loops through the gallery array
+    for (let i = 0; i < photoCollection.length; i++) {
+
+        // Checks the current opened photo and if it's not the first image
+        if (largeImage.src == photoCollection[i].src &&
+            i != 0) {
+            // Updates previous image in the gallery
+            prevImage.src = photoCollection[i - 1].src;
+            prevImage.alt = photoCollection[i - 1].alt;
+        }
+
+        // Checks the current opened photo and if it's the first image
+        else if (largeImage.src == photoCollection[i].src) {
+
+            // Updates previous photo to the last in the gallery
+            prevImage.src = lastImage.src;
+            prevImage.alt = lastImage.alt;
+        }
+    };
+
+    // Updates the opened image
+    [largeImage.src, largeImage.alt] = [prevImage.src, prevImage.alt];
+};
+
+
+// Function to close the large image.
 closeX.addEventListener("click", function () {
+    // Updates display to none
     document.getElementById("fullImgBox").style.display = "none";
 });
