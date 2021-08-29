da5bb1970eb1ed4b687a3906fd7b0faff6d13a32
m-binda <marcelobinda@gmail.com>
2021-08-28 22:09:45 -0300

    Adjusted index for smaller screens

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,15 @@
+// Add functionality to open and close the menu by clicking at the
+// hamburger icon.
+
+// Gets the burger icon element
 let menuBurger = document.getElementById("menuBurger");
 
+// Adds an event listener for the burger icon element
 menuBurger.addEventListener("click", function () {
+    // Gets the nav bar
     let siteMenu = document.getElementById("nav-bar");
+
+    // Opens and closes the nav bar depending on its initial display value
     if (siteMenu.style.display === "block") {
         document.getElementById("nav-bar").style.display = "none";
     } else {
